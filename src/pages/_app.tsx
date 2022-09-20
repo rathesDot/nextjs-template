174e60e21402f08b4f0809d05a3c4c10bd7e66e3
Rathes Sachchithananthan <rathes@aheenam.com>
2022-09-20 09:44:48 +0100

    ✨ Load fathom if code is provided

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,35 @@
-import "../../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import * as Fathom from "fathom-client";
+
+import "../../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!process.env.FATHOM_TRACKING_CODE) {
+      return;
+    }
+
+    Fathom.load(process.env.FATHOM_TRACKING_CODE, {
+      includedDomains: (process.env.FATHOM_INCLUDED_DOMAINS || "").split(","),
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
